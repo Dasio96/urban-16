3756558baa88290f7d4569396234d0697e208e9d
Dasio96 <dawid01852@gmail.com>
2026-09-18 02:02:00 +0200

    feat(cpu): implement fetch-decode loop skeleton

--- lc3.c
+++ lc3.c
@@ -6,3 +6,16 @@ void lc3_init(lc3_cpu *cpu) {
   cpu->pc = 0x3000;
   cpu->cond = FL_ZRO;
 }
+
+void lc3_step(lc3_cpu *cpu) {
+  uint16_t instr = cpu->ram[cpu->pc];
+  cpu->pc++;
+  uint16_t opcode = instr >> 12;
+
+  switch (opcode) {
+  case OP_ADD:
+    break;
+  default:
+    break;
+  }
+}
--- lc3.h
+++ lc3.h
@@ -9,6 +9,25 @@ enum {
   FL_NEG = 1 << 2,
 };
 
+enum {
+  OP_BR = 0,
+  OP_ADD,
+  OP_LD,
+  OP_ST,
+  OP_JSR,
+  OP_AND,
+  OP_LDR,
+  OP_STR,
+  OP_RTI,
+  OP_NOT,
+  OP_LDI,
+  OP_STI,
+  OP_JMP,
+  OP_RES,
+  OP_LEA,
+  OP_TRAP
+};
+
 typedef struct {
   uint16_t ram[65536];
   uint16_t reg[8];
@@ -17,5 +36,6 @@ typedef struct {
 } lc3_cpu;
 
 void lc3_init(lc3_cpu *cpu);
+void lc3_step(lc3_cpu *cpu);
 
 #endif // !LC3_H
