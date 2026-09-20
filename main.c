c3df51d6223c1e279180c29b42aa714203df4050
Dasio96 <dawid01852@gmail.com>
2026-09-20 04:26:05 +0200

    test(main): add inline LC-3 assembly program to verify CPU execution

--- main.c
+++ main.c
@@ -1,2 +1,27 @@
+#include "lc3.h"
 #include <stdio.h>
-int main() { printf("bass"); }
+
+int main(void) {
+  lc3_cpu cpu;
+  lc3_init(&cpu);
+
+  uint16_t start_address = 0x3000;
+
+  cpu.ram[start_address + 0] = 0xE002;
+  cpu.ram[start_address + 1] = 0xF022;
+  cpu.ram[start_address + 2] = 0xF025;
+
+  cpu.ram[start_address + 3] = 'U';
+  cpu.ram[start_address + 4] = 'R';
+  cpu.ram[start_address + 5] = 'B';
+  cpu.ram[start_address + 6] = 'A';
+  cpu.ram[start_address + 7] = 'N';
+  cpu.ram[start_address + 8] = '\n';
+  cpu.ram[start_address + 9] = '\0';
+
+  while (cpu.running) {
+    lc3_step(&cpu);
+  }
+
+  return 0;
+}
